9b0d6f4d9b85d435ce46106c7592e5c556ce7dcb
iamchenxin <iamchenxin@gmail.com>
2016-04-14 16:43:02 +0000

    SQL tagged template for postgres

---
+++ src/__tests__/sql-test.js
@@ -0,0 +1,41 @@
+import SQL from '../sql.js';
+import {inst} from '../sql.js';
+
+describe('test SQL tagged template', () => {
+  let userId;
+  let password;
+
+  beforeEach(() => {
+    userId = '001';
+    password = '123456';
+  });
+
+  it('test empty string', () => {
+    expect(SQL``)
+    .toEqual({
+      text: '',
+      values: []
+    });
+  });
+
+  it('test string with params', () => {
+    expect(SQL`select name from user where id=${userId} and password=${password}`)
+    .toEqual({
+      text: 'select name from user where id=$1 and password=$2',
+      values: [userId, password]
+    });
+  });
+
+  it('test directly insert value [inst]', () => {
+    let column = 'name';
+    let table = 'admin';
+    expect(SQL`select ${inst(column)} from ${inst(table)}
+where id=${userId} and password=${password}`)
+    .toEqual({
+      text: `select "name" from "admin"
+where id=$1 and password=$2`,
+      values: [userId, password]
+    });
+  });
+
+});
